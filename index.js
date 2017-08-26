06d7a922c2503cf864f9f1b1cbb990a704e268ab
shailendrabhargava93 <shailendrabhargavaa@gmail.com>
2017-08-26 21:49:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){ 
     
     $(window).scroll(function(){ 
-        if ($(this).scrollTop() > 100) { 
+        if ($(this).scrollTop() > 200) { 
             $('#scroll').fadeIn(); 
         } else { 
             $('#scroll').fadeOut(); 
